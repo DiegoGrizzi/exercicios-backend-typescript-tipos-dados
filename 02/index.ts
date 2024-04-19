77ad2b4aa53e3d51ded559c21178755649cf0361
Diego Grizzi <diegogrizzi@hotmail.com>
2024-04-19 15:01:10 -0300

    Filtro realizado.

---
+++ 02/index.ts
@@ -0,0 +1,48 @@
+const dadosUsuarios = [
+    {
+        nome: "Guido",
+        idade: 32,
+        status: true,
+    },
+    {
+        nome: "Dani",
+        idade: 30,
+        status: true,
+    },
+    {
+        nome: "João",
+        idade: 40,
+        status: false,
+    },
+    {
+        nome: "Guilherme",
+        idade: 29,
+        status: true,
+    },
+    {
+        nome: "Ana",
+        idade: 18,
+        status: false,
+    },
+    {
+        nome: "José",
+        idade: 28,
+        status: false,
+    },
+]
+
+type Usuario={
+    nome:string,
+    idade:number,
+    status:boolean
+}
+
+
+const filtrarUsuarios= (dadosUsuarios:Usuario[],nome:string):Usuario[]=>{
+
+    const nomeUsuario=nome.toLowerCase()
+
+    return dadosUsuarios.filter(usuario=>usuario.nome.toLowerCase().includes(nomeUsuario))
+}
+
+console.log(filtrarUsuarios(dadosUsuarios,'diego'));
